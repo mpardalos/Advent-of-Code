94bf18263519a2bd0ee297591221854cd026f764
Michalis Pardalos <m.pardalos@gmail.com>
2020-12-09 18:58:38 +0000

    Add mechanism for solutions to have extras
    Allows for stuff like visualizations

--- src/Common.hs
+++ src/Common.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Common where
 
+import Data.Map (Map)
+import Text.Parsec
+import Data.Functor.Identity
+
 type Solution = String -> IO ()
+
+-- | Other functions to offer on the cli
+type Extra = String -> IO ()
+type Extras = Map String (String -> IO ())
 
 at :: [a] -> Int -> Maybe a
 at [] _ = Nothing
--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,27 @@
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE LambdaCase #-}
-module Main where
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
 
-import Text.Read (readMaybe)
-import System.Environment (getArgs)
-import System.Exit (ExitCode(ExitFailure), exitWith)
-import System.Directory (doesFileExist)
+module Main where
 
 import Common
 import qualified Day4
 import qualified Day5
 import qualified Day6
 import qualified Day8
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+import Text.Parsec
+import Text.Read (readMaybe)
+import Data.Maybe
+import Control.Monad
+import Data.List
+import Text.Parsec.Error (errorMessages)
+import qualified Data.Map as Map
 
 unsolved :: Solution
 unsolved _ = putStrLn "There is no solution for this problem"
@@ -25,27 +34,51 @@ solutionFor 6 = Day6.solve
 solutionFor 8 = Day8.solve
 solutionFor _ = unsolved
 
+extrasFor :: Int -> Extras
+extrasFor 8 = Day8.extras
+extrasFor _ = Map.empty
+
 usage :: IO ()
 usage = putStrLn "Usage: aoc2020 <day> [input_file]"
 
 fileDoesNotExist :: String -> IO ()
 fileDoesNotExist fn = putStrLn ("File does not exist: " ++ fn)
 
-dayInput :: Int -> String
-dayInput n = "inputs/day" ++ show n
+type ArgParser = Parsec [String] ()
 
-parseArgs :: [String] -> IO (Int, String)
-parseArgs ((readMaybe @Int -> Just day):rest) =
-  let inputPath = case rest of
-        [inputPath] -> inputPath
-        [] -> dayInput day
-  in doesFileExist inputPath >>= \case
-    True -> return (day, inputPath)
-    False -> fileDoesNotExist inputPath >> exitWith (ExitFailure 1)
-parseArgs _ = usage >> exitWith (ExitFailure 1)
+data Args = Args
+  { day :: Int,
+    inputFile :: String,
+    method :: Maybe String
+  }
+
+argsP :: ArgParser Args
+argsP = do
+  Just day <- readMaybe @Int <$> arg
+  method <- optionMaybe (try (arg `matching` stripPrefix "--"))
+  inputFile <- arg `orDefault` dayInput day
+
+  return Args{..}
+  where
+    arg = anyToken
+    p `orDefault` def = fromMaybe def <$> optionMaybe p
+    dayInput n = "inputs/day" ++ show n
+    matching p f = f <$> p >>= \case
+      Just val -> pure val
+      Nothing -> fail ""
+
+parseArgs = runParser argsP () "args"
 
 main :: IO ()
 main = do
-  (day, inputPath) <- parseArgs =<< getArgs
-  inputContents <- readFile inputPath
-  solutionFor day inputContents
+  Args {..} <- (parseArgs <$> getArgs) >>= \case
+    Right args -> pure args
+    Left err -> usage >> exitWith (ExitFailure 1)
+
+  inputContents <- readFile inputFile
+
+  case method of
+    Nothing -> solutionFor day inputContents
+    Just method -> case Map.lookup method (extrasFor day) of
+      Just extra -> extra inputContents
+      Nothing -> putStrLn ("There is no " ++ method ++ " method for day " ++ show day)
